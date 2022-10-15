189d6b809123d50bce62b3f94988597ed0f0ae2d
Aleksandr Anenkov <aleksandr.anenkov@intel.com>
2022-10-15 22:00:17 +0300

    Add help message

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import threading
 import time
 import yaml
+from retrying import retry
 
 token = None
 admin_chat_id = None
@@ -112,13 +113,13 @@ def is_supported_ticker(ticker: str):
     return False
 
 def send_news_message(chat_id, title: str, announce: str, link: str, ticker: str):
-    print(f"Send to {chat_id}")
-    msg = "<b>" + title + "</b>\n\n"
-    if announce is not None and announce != "":
-        msg += announce + "\n\n"
-    msg += "#" + ticker.upper()
-    if "дивиденды" in title:
-        msg += " #дивиденды"
+    print(f'Send to {chat_id}')
+    msg = f'<b>{title}</b>\n\n'
+    if announce is not None and announce != '':
+        msg += f'{announce}\n\n'
+    msg += f'#{ticker.upper()}'
+    if 'дивиденды' in title:
+        msg += ' #дивиденды'
     reply_markup=types.InlineKeyboardMarkup([
         [types.InlineKeyboardButton(text='Open link', url=domain_name + link)],
     ])
@@ -131,46 +132,39 @@ def broadcast_news(user_list, title: str, announce: str, link: str, ticker: str)
         send_news_message(chat_id, title, announce, link, ticker)
 
 def get_user_list_for_news_broadcast(ticker: str):
-    db_ticker_name = ticker.upper() + "_ticker"
-    db_cursor.execute("SELECT chat_id FROM Users WHERE " + db_ticker_name + " = ?", (1,))
+    db_ticker_name = ticker.upper() + '_ticker'
+    db_cursor.execute(f'SELECT chat_id FROM Users WHERE {db_ticker_name} = 1')
     return db_cursor.fetchone()
 
 def handle_news(titles, announces, links, ticker: str):
-    # assert len(titles) == len(links)
-    if len(titles) != len(links):
-        print(f"{ticker}: len(titles) != len(announces)")
-
     count = min(len(titles), len(links))
     for i in range(count):
         title = titles[i].get_text()
-        announce = ""
+        announce = ''
         if len(announces) > 0:
             announce = announces[i].get_text()
         link = links[i]['href']
-        today_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        db_cursor.execute("INSERT OR IGNORE INTO News (link, title, announce, date, ticker) VALUES (?, ?, ?, ?, ?)", (link, title, announce, today_date, ticker))
+        today_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        db_cursor.execute('INSERT OR IGNORE INTO News (link, title, announce, date, ticker) VALUES (?, ?, ?, ?, ?)', (link, title, announce, today_date, ticker))
         if db_cursor.rowcount > 0:
-            print(f"New: {title}")
+            print(f'New: {title}')
             user_list = get_user_list_for_news_broadcast(ticker)
             broadcast_news(user_list, title, announce, link, ticker)
     db.commit()
 
-def get_web_page(url: str):
-    retries = 1
-    try:
-        page = requests.get(url)
-        page.raise_for_status()
-        return page
-    except Exception as e:
-        wait = retries * 30
-        print(f"Error getting web page. Wait {wait} sec and re-trying...")
-        time.sleep(wait)
-        retries += 1
+def retry_if_connection_error(exception):
+    print('Trying send request again...')
+    return isinstance(exception, ConnectionError)
+
+# if exception retry with 30 second wait  
+@retry(retry_on_exception=retry_if_connection_error, wait_fixed=30000)
+def safe_request(url, **kwargs):
+    return requests.get(url, **kwargs)
 
 def parse_web_and_insert_to_db(ticker: str):
-    url = domain_name + "/invest/etfs/" + ticker + "/news/"
-    page = get_web_page(url)
-    soup = BeautifulSoup(page.content, "html.parser")
+    url = f'{domain_name}/invest/etfs/{ticker}/news/'
+    page = safe_request(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
     wrappers = soup.select('div[class*="NewsItem__wrapper_"]')
     for w in wrappers:
         titles = w.select('div[class*="NewsItem__title_"]')
@@ -204,7 +198,7 @@ def handle_update_msg(msg, res=False):
 
 @bot.message_handler(commands=["stop"])
 def handle_stop_msg(msg, res=False):
-    bot.send_message(msg.chat.id, "Bye " + msg.chat.first_name)
+    bot.send_message(msg.chat.id, f'Bye {msg.chat.first_name}')
     delete_user_from_db(msg.chat.id)
 
 @bot.message_handler(commands=["add"])
@@ -235,6 +229,14 @@ def handle_remove_msg(msg, res=False):
     else:
         bot.send_message(msg.chat.id, "Use /remove <ticker>")
 
+@bot.message_handler(commands=["help"])
+def handle_help_msg(msg, res=False):
+    text = "Команды:\n"
+    text += "/add <тикер>\n"
+    text += "/remove <тикер>\n\n"
+    text += "Список поддерживаемых тикеров: " + supported_tickers
+    bot.send_message(msg.chat.id, text)
+
 @bot.message_handler(commands=["clear"])
 def handle_clear_msg(msg, res=False):
     if msg.chat.id != admin_chat_id:
@@ -259,7 +261,7 @@ f_stop = threading.Event()
 update_news_async(f_stop)
 
 print("bot online")
-bot.polling(none_stop=True, interval=0)
+bot.infinity_polling(timeout=10, long_polling_timeout=5)
 
 f_stop.set()
 db.close()
